17196358dbcc0c7cdd277c04cb6f7d66809d8440
Magomed Saybulaev <MS>
2023-02-04 23:52:36 +0300

    add complete email order - test (1)

--- www/wp-content/themes/awakening-v2/woocommerce/emails/customer-completed-order.php
+++ www/wp-content/themes/awakening-v2/woocommerce/emails/customer-completed-order.php
@@ -10,205 +10,43 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 echo $email_heading . "\n\n";
 
-$status = $order->get_status();
-$items = $order->get_items();
-if ($status == "completed") {
-    foreach ( $items as $item ) {
-        $product_id = $item['product_id'];
+var_dump($order);
 
-        //mentor
-        if ( in_array( $item['product_id'], 13 ) ) {
-            ?>
-            <style>
-                @font-face {
-                    font-family: 'Poppins';
-                    font-style: normal;
-                    font-weight: 400;
-                    font-display: swap;
-                    src: url(https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJbecmNE.woff2) format('woff2');
-                    unicode-range: U+0900-097F, U+1CD0-1CF6, U+1CF8-1CF9, U+200C-200D, U+20A8, U+20B9, U+25CC, U+A830-A839, U+A8E0-A8FB;
-                }
-                /* latin-ext */
-                @font-face {
-                    font-family: 'Poppins';
-                    font-style: normal;
-                    font-weight: 400;
-                    font-display: swap;
-                    src: url(https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJnecmNE.woff2) format('woff2');
-                    unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-                }
-                /* latin */
-                @font-face {
-                    font-family: 'Poppins';
-                    font-style: normal;
-                    font-weight: 400;
-                    font-display: swap;
-                    src: url(https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJfecg.woff2) format('woff2');
-                    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-                }
+//echo sprintf( __( "Hi there. Your recent order on %s has been completed. Your order details are shown below for your reference:", 'woocommerce' ), get_option( 'blogname' ) ) . "\n\n";
 
-                .email {
-                    background: #2F3038;
-                    background-image: url(https://awakening-online.ru/wp-content/themes/awakening-v2/images/bg.png);
-                    background-repeat: no-repeat;
-                    width: 100%;
-                    height: 100%;
-                    padding: 40px;
-                    box-sizing: border-box;
-                }
-                .email__logo {
-                    display: block;
-                    width: 100%;
-                    text-align: center;
-                    padding: 0 0 50px 0;
-                    box-sizing: border-box;
-                }
-                .email__box {
-                    background: #202027;
-                    border: 1px solid #F9F7F7;
-                    border-radius: 30px;
-                    padding: 40px;
-                    outline: 0;
-                    box-sizing: border-box;
-                    margin-bottom: 60px;
-                }
-                .text {
-                    color: white;
-                    font-size: 25px;
-                }
-                * {
-                    margin: 0;
-                    padding: 0;
-                    font-family: 'Poppins', sans-serif;
-                    overflow-wrap: normal !important;
-                    font-style: normal;
-                    font-weight: 100;
-                    word-break: break-word;
-                }
-                a {
-                    font-style: normal;
-                    font-weight: 600;
-                    font-size: 21px;
-                    line-height: 36px;
-                    text-decoration: none;
-                    transition: .2s ease-out;
-                    color: #D8A72B;
-                }
-            </style>
-            <div class="email">
-                <a class="email__logo" href="https://awakening-online.ru/">
-                    <img src="https://awakening-online.ru/wp-content/themes/awakening-v2/images/logo.png">
-                </a>
-                <div class="email__box text">
-                    Благодарим вас за покупку курса «Mentor».<br>
-                    Сначала вы вместе c близким просматриваете курс Awakening.<br>
-                    Ссылка для просмотра курса «Awakening»<br>
-                    <a href="https://youtu.be/awPyD8AkAko">https://youtu.be/awPyD8AkAko</a><br>
-                    ПРОСМОТР КУРСА «Mentor» ТОЛЬКО ДЛЯ НАСТАВНИКА<br>
-                    Затем вы просматриваете курс Mentor<br>
-                    Ссылка для просмотра курса «Mentor».<br>
-                    <a href="https://youtu.be/0-33A019op8">https://youtu.be/0-33A019op8</a><br>
-                    Экзамен <a href="https://youtu.be/dcmUGJmF0vE">https://youtu.be/dcmUGJmF0vE</a><br>
-                    Медитация <a href="https://youtu.be/o2JnpWqWnmU">https://youtu.be/o2JnpWqWnmU</a><br>
-                    По всем вопросам обращайтесь в телегам <a href="https://t.me/awaken_supp">@awaken_supp</a><br>
-                    Удачного пути к пробуждению.<br>
-                </div>
-            </div>
-            <?php
-        }
+//echo "****************************************************\n\n";
 
-        //awakening
-        if ( in_array( $item['product_id'], 12 ) ) {
-            ?>
-            <style>
-                @font-face {
-                    font-family: 'Poppins';
-                    font-style: normal;
-                    font-weight: 400;
-                    font-display: swap;
-                    src: url(https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJbecmNE.woff2) format('woff2');
-                    unicode-range: U+0900-097F, U+1CD0-1CF6, U+1CF8-1CF9, U+200C-200D, U+20A8, U+20B9, U+25CC, U+A830-A839, U+A8E0-A8FB;
-                }
-                /* latin-ext */
-                @font-face {
-                    font-family: 'Poppins';
-                    font-style: normal;
-                    font-weight: 400;
-                    font-display: swap;
-                    src: url(https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJnecmNE.woff2) format('woff2');
-                    unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-                }
-                /* latin */
-                @font-face {
-                    font-family: 'Poppins';
-                    font-style: normal;
-                    font-weight: 400;
-                    font-display: swap;
-                    src: url(https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJfecg.woff2) format('woff2');
-                    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-                }
+//do_action( 'woocommerce_email_before_order_table', $order, false );
 
-                .email {
-                    background: #2F3038;
-                    background-image: url(https://awakening-online.ru/wp-content/themes/awakening-v2/images/bg.png);
-                    background-repeat: no-repeat;
-                    width: 100%;
-                    height: 100%;
-                    padding: 40px;
-                    box-sizing: border-box;
-                }
-                .email__logo {
-                    display: block;
-                    width: 100%;
-                    text-align: center;
-                    padding: 0 0 50px 0;
-                    box-sizing: border-box;
-                }
-                .email__box {
-                    background: #202027;
-                    border: 1px solid #F9F7F7;
-                    border-radius: 30px;
-                    padding: 40px;
-                    outline: 0;
-                    box-sizing: border-box;
-                    margin-bottom: 60px;
-                }
-                .text {
-                    color: white;
-                    font-size: 25px;
-                }
-                * {
-                    margin: 0;
-                    padding: 0;
-                    font-family: 'Poppins', sans-serif;
-                    overflow-wrap: normal !important;
-                    font-style: normal;
-                    font-weight: 100;
-                    word-break: break-word;
-                }
-                a {
-                    font-style: normal;
-                    font-weight: 600;
-                    font-size: 21px;
-                    line-height: 36px;
-                    text-decoration: none;
-                    transition: .2s ease-out;
-                    color: #D8A72B;
-                }
-            </style>
-            <div class="email">
-                <a class="email__logo" href="https://awakening-online.ru/">
-                    <img src="https://awakening-online.ru/wp-content/themes/awakening-v2/images/logo.png">
-                </a>
-                <div class="email__box text">
-                    Благодарим вас за покупку курса «Awakening».<br>
-                    Ссылка для просмотра курса <a href="https://youtu.be/Vm7HBz0WRgg">https://youtu.be/Vm7HBz0WRgg</a><br>
-                    Медитация <a href="https://youtu.be/o2JnpWqWnmU">https://youtu.be/o2JnpWqWnmU</a><br>
-                    По всем вопросам обращайтесь в телегам <a href="https://t.me/awaken_supp">@awaken_supp</a><br>
-                    Удачного пути к пробуждению.<br>
-                </div>
-            </div>
-            <?php
-        }
-    }
-}
+//echo sprintf( __( 'Order number: %s', 'woocommerce'), $order->get_order_number() ) . "\n";
+//echo sprintf( __( 'Order date: %s', 'woocommerce'), date_i18n( woocommerce_date_format(), strtotime( $order->order_date ) ) ) . "\n";
+
+//do_action( 'woocommerce_email_order_meta', $order, false, true );
+
+//echo "\n" . $order->email_order_items_table( true, false, true, '', '', true );
+
+//echo "----------\n\n";
+
+//if ( $totals = $order->get_order_item_totals() ) {
+//    foreach ( $totals as $total ) {
+//        echo $total['label'] . "\t " . $total['value'] . "\n";
+//    }
+//}
+
+//echo "\n****************************************************\n\n";
+
+//do_action( 'woocommerce_email_after_order_table', $order, false, true );
+
+//echo __( 'Your details', 'woocommerce' ) . "\n\n";
+
+//if ( $order->billing_email )
+//    echo __( 'Email:', 'woocommerce' ); echo $order->billing_email. "\n";
+
+//if ( $order->billing_phone )
+//    echo __( 'Tel:', 'woocommerce' ); ?><!-- --><?php //echo $order->billing_phone. "\n";
+
+//woocommerce_get_template( 'emails/plain/email-addresses.php', array( 'order' => $order ) );
+
+//echo "\n****************************************************\n\n";
+
+//echo apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) );
